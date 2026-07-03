96d442e8d3c8ad8214c8f9cc56713c8f2846b0e1
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-03 23:06:18 +0300

    mount order routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -45,7 +45,7 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/nav", require("./routes/navRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
-// app.use('/api/orders', require('./routes/orderRoutes'));
+app.use("/api/orders", require("./routes/orderRoutes"));
 // app.use('/api/reviews', require('./routes/reviewRoutes'));
 // app.use('/api/wishlist', require('./routes/wishlistRoutes'));
 // app.use('/api/coupons', require('./routes/couponRoutes'));
