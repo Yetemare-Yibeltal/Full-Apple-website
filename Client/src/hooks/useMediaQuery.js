cbeed544ebd6bb474655db8f1bbb6187fe136874
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-09 00:32:37 +0300

    add media query hook

---
+++ Client/src/hooks/useMediaQuery.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches,
+  );
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    const listener = (e) => setMatches(e.matches);
+
+    mediaQueryList.addEventListener("change", listener);
+    return () => mediaQueryList.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
+}
